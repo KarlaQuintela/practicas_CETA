ea4f8d90ddc50ccca6cc37f4ae3c9d87f1a9ed2e
KarlaQuintela <karlieqf03@gmail.com>
2024-03-05 17:43:40 -0500

    admin.py ceta updated (register models)

--- Gestion_CETA/ceta/admin.py
+++ Gestion_CETA/ceta/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from ceta.module_accounting.models import *
+from ceta.module_contract.models import *
+from ceta.module_human_resources.models import *
+from ceta.module_offer.models import *
+from ceta.module_user.models import *
 
 # Register your models here.
+admin.site.register(Bill)
+admin.site.register(Receipt)
+admin.site.register(Remuneration)
+
+admin.site.register(Client)
+admin.site.register(Contract)
+admin.site.register(PaymentTerm)
+admin.site.register(PaymentWorker)
+admin.site.register(Expense)
+
+admin.site.register(Category)
+admin.site.register(Worker)
+
+admin.site.register(User)
+admin.site.register(Role)
