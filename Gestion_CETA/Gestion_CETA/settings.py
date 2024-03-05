4f0fd78be1b0ba954164c4993ff0ecfed09d00a7
KarlaQuintela <karlieqf03@gmail.com>
2024-03-04 23:24:03 -0500

    conected to postgres and worker and category models donde and migrated

--- Gestion_CETA/Gestion_CETA/settings.py
+++ Gestion_CETA/Gestion_CETA/settings.py
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'Gestion_CETA.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db_gestion_ceta",
+        "USER": "postgres",
+        "PASSWORD": "3517",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- Gestion_CETA/Human_Resources/models.py
+++ Gestion_CETA/Human_Resources/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id_cg = models.IntegerField(primary_key=True)
+    name_cg = models.CharField(max_length=255)
+    hourly_wage_cg = models.FloatField()
+
+    def __str__(self):
+        return self.name_cg
+
+class Worker(models.Model):
+    id_w = models.CharField(max_length=11, primary_key=True)
+    fk_id_cg = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name_w = models.CharField(max_length=255)
+    address_w = models.CharField(max_length=255)
+    phone_w = models.CharField(max_length=255)
+    email_w = models.CharField(max_length=255)
+    department_w = models.CharField(max_length=255)
+    num_account_w = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.name_w
