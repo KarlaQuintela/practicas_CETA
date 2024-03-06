6945ab89636dbb9c5adaccd945133a6a6b949ce3
KarlaQuintela <karlieqf03@gmail.com>
2024-03-06 03:07:28 -0500

    link entre listas

--- Gestion_CETA/ceta/urls.py
+++ Gestion_CETA/ceta/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .module_human_resources import views
 
 urlpatterns = [
-    path('',  views.list_workers, name="index"),
-    path("<int:id_worker>", views.get_worker, name="worker"),
+    path('',  views.list_workers, name="indexTrabajador"),
+    path("<int:id_worker>", views.get_worker, name="workerDetail"),
 ]
