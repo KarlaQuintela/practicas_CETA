0aad8a5e20dfb5537193e13f05797c4414628d61
KarlaQuintela <karlieqf03@gmail.com>
2024-05-30 23:44:55 -0400

    select_related actualizao

--- Gestion_CETA/ceta/module_reports/views.py
+++ Gestion_CETA/ceta/module_reports/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
+from django.db.models import Q
 from rest_framework.response import Response
 from .serializers import *
 from ceta.module_contract.models import Contract
 
 class ReportsViewset(viewsets.GenericViewSet):
      def clients_contracts(self, request, id):
-        contracts = Contract.objects.filter(fk_id_client=id)
+        contracts = Contract.objects.select_related('Client').filter(fk_id_client=id)
         serializer = ContractSerializer(contracts, many=True)
         return Response(serializer.data)
+       
