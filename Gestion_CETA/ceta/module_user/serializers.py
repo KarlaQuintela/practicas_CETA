2175da8373eaa998905006d77af773de214dc0e2
Alfredo Hernández Rodríguez <111950884+RedAlf121@users.noreply.github.com>
2024-05-30 21:27:44 -0400

    Default Django User validate this attributes

--- Gestion_CETA/ceta/module_user/serializers.py
+++ Gestion_CETA/ceta/module_user/serializers.py
@@ -30,31 +30,8 @@ class RoleSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     fk_id_role = serializers.PrimaryKeyRelatedField(queryset = Role.objects.all(), many=False)
-     
     class Meta:
         model = User
         fields = '__all__'      
         read_only_fields = ('id_user',)
 
-    def validate(self, res: OrderedDict):
-            name_user = res.get('name_user')   
-            password_user = res.get('password_user')
-
-            # Validate name_user
-            if not is_valid_alpha(name_user):
-                raise serializers.ValidationError({
-                    'name_user': 'This field should contain only alphabetic characters.'
-                })
-            if not is_valid_characters_count(name_user, 3):
-                raise serializers.ValidationError({
-                    'name_user': 'This field should contain more than 3 characters.'
-                })
-            
-            #Validate password_user
-            if not is_valid_characters_count(password_user, 7):
-                raise serializers.ValidationError({
-                    'password_user': 'This field should contain more than 7 characters.'
-                })
-            
-            return res
-        
